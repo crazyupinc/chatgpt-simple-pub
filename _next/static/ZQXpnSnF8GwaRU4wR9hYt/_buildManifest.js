e07356d4b212098ba737e29e2e48bf2ae17952d3
kiban18 <>
2023-06-05 22:08:37 +0000

    Updates

---
+++ _next/static/ZQXpnSnF8GwaRU4wR9hYt/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1157f632eea663c7.css","static/chunks/pages/index-1218ef8a1c8c3885.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
